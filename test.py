ba3173e22dd6a581df572efa3e41c5274784ffb6
ivanya123 <aplesnin2010@yandex.ru>
2025-02-09 23:29:18 +0300

    123

--- test.py
+++ test.py
@@ -1,8 +1,29 @@
+import json
 import tkinter as tk
 from tkinter.messagebox import showinfo, showerror
 from tkinter.filedialog import askopenfilename, askdirectory
 from function import Password, ListPassword
 from tkinter import ttk
+import os
+
+
+class InputTkinter(tk.Toplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        self.title('Введите ваше имя')
+        self.geometry('300x100')
+        self.name = None
+        self.input_field = tk.Entry(self, width=30)
+        self.input_field.pack()
+        self.button = tk.Button(self, text='Отправить', command=self.send_name)
+        self.button.pack()
+
+    def send_name(self):
+        self.name = self.input_field.get()
+        if self.name:
+            self.quit()
+        else:
+            showerror('Ошибка', 'Введите имя')
 
 
 def on_mouse_wheel(event, canvas):
@@ -12,13 +33,39 @@ def on_mouse_wheel(event, canvas):
         canvas.yview_scroll(1, "units")
 
 
+def first_start(master):
+    if os.path.exists('config.json'):
+        with open('config.json', 'r') as file:
+            data = json.load(file)
+            name = data['name']
+
+        list_password = ListPassword(name)
+        list_password.from_json()
+        return list_password, name
+
+    else:
+        input_window = InputTkinter(master)
+        input_window.mainloop()
+        name = input_window.name
+        with open('config.json', 'w') as file:
+            json.dump({'name': name}, file)
+        list_password = ListPassword(name=name)
+        list_password.to_json()
+        input_window.destroy()
+        master.wm_attributes('-topmost', 1)
+        return list_password, name
+
+
 class MainApp(tk.Tk):
-    def __init__(self, name: ListPassword):
+    def __init__(self):
         super().__init__()
-        self.list_password = name
-        self.name = name.name
+        list_password, name = first_start(self)
+        self.list_password: ListPassword = list_password
+        self.name: str = name
         self.title(f'Manager_Password_{self.name}')
         self.geometry('600x500')
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
         self.create_widgets()
 
     def create_widgets(self, company=None):
@@ -26,22 +73,23 @@ class MainApp(tk.Tk):
         self.scrollbar = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
 
         frame_vision_password = tk.Frame(self.canvas)
-        frame_vision_password.pack(fill='x', padx=10, pady=10)
-        self.canvas.create_window((0, 0), anchor='center', window=frame_vision_password)
+        frame_vision_password.pack(fill='both', padx=10, pady=10)
+        self.canvas_window = self.canvas.create_window((0, 0), anchor='nw', window=frame_vision_password)
         frame_vision_password.bind(
             "<Configure>",
             lambda e: self.canvas.configure(
                 scrollregion=self.canvas.bbox("all")
             )
         )
+        self.canvas.bind("<Configure>", self.on_canvas_configure)
         self.canvas.bind_all("<MouseWheel>", lambda event: on_mouse_wheel(event, self.canvas))
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        self.canvas.pack(side="top", fill='x', expand=True)
-        self.scrollbar.pack(side="right", fill="x")
+        self.canvas.grid(column=0, row=0, sticky='nsew')
+        self.scrollbar.grid(column=1, row=0, sticky='ns')
         self.create_widget_password(frame_vision_password, company)
 
         self.frame_add_password = tk.LabelFrame(self, text='Add Password')
-        self.frame_add_password.pack(fill='x', padx=10, pady=10)
+        self.frame_add_password.grid(column=0, row=1, padx=10, pady=10, columnspan=2)
         tk.Label(self.frame_add_password, text='Company, website:').grid(column=0, row=0, padx=5, pady=5)
         self.company_entry = tk.Entry(self.frame_add_password)
         self.company_entry.grid(column=1, row=0, padx=5, pady=5)
@@ -54,12 +102,16 @@ class MainApp(tk.Tk):
         self.button_create_password = tk.Button(self.frame_add_password, text='Create', command=self.create_password)
         self.button_create_password.grid(column=1, row=3, padx=5, pady=5)
         self.search_widget = ttk.Combobox(self.frame_add_password,
-                                          values=list(set([passw.company for passw in self.list_password.list_pass])),
+                                          values=sorted(list(set([passw.company for passw in self.list_password.list_pass]))),
                                           state='normal')
         self.search_widget.grid(column=2, row=0, padx=5, pady=5)
         self.search_button = tk.Button(self.frame_add_password, text='Search',
                                        command=self.filter_password)
         self.search_button.grid(column=2, row=1, padx=5, pady=5)
+
+    def on_canvas_configure(self, event):
+        # При изменении размера Canvas обновляем ширину внутреннего окна
+        self.canvas.itemconfig(self.canvas_window, width=event.width)
 
     def create_password(self):
         if self.password_entry.get() and self.login_entry.get() and self.company_entry.get():
@@ -74,7 +126,7 @@ class MainApp(tk.Tk):
 
     def refresh_display(self):
         self.destroy()
-        new_app = MainApp(self.list_password)
+        new_app = MainApp()
         new_app.mainloop()
 
     def delete_password(self, i):
@@ -82,6 +134,8 @@ class MainApp(tk.Tk):
         self.refresh_display()
 
     def create_widget_password(self, frame: tk.Frame, company: str = None):
+        self.list_password.list_pass.sort(key=lambda x: x.company)
+
         if company is None:
             i = 0
             for password in self.list_password.list_pass:
@@ -120,7 +174,8 @@ class MainApp(tk.Tk):
             self.create_widgets()
 
 
-new_pass = ListPassword('Ivan')
-new_pass.from_json()
-app = MainApp(new_pass)
+
+app = MainApp()
 app.mainloop()
+
+
